d222ba8017023388277611bd207de5c5563232b0
Anton <stormantonio.che@gmail.com>
2016-11-28 21:53:24 +0200

    Resume storage (first commit)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,91 @@
+import com.sun.org.apache.regexp.internal.RE;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final Integer storageMaxLength = 10000;
+    Resume[] storage = new Resume[storageMaxLength];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        if (isFull()){
+            System.out.println("Хранилище резюме переполнено!!!");
+            return;
+        }
+        if (find(r.getUuid()) != -1) {
+            System.out.println("Извините, такое " + r + " резюме уже есть!!!");
+            return;
+        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        if (isNull())
+            return null;
+        int findUuid = find(uuid);
+        if (findUuid != -1)
+            return storage[findUuid];
+        System.out.println("Такого " + uuid + " резюме нет!!!");
         return null;
     }
 
     void delete(String uuid) {
+        if (isNull())
+            return;
+        int findUuid = find(uuid);
+        if (findUuid != -1){
+            storage[findUuid] = storage[size - 1];
+            size--;
+            return;
+        }
+        System.out.println("Такого " + uuid + " резюме нет!!!");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    private boolean isFull(){
+        if (size == storageMaxLength)
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isNull() {
+        if (size == 0){
+            System.out.println("Хранилище резюме пустое!!!");
+            return true;
+        }
+        return false;
+    }
+
+    private int find(String currentUuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(currentUuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
